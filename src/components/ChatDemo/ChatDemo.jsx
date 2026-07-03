22ea0f03e23a4da45f3e9f17940e2e3bf05905c2
Prath1507 <117733234+Prath1507@users.noreply.github.com>
2026-07-03 19:04:37 +0530

    fix conversation import for vercel

--- src/components/ChatDemo/ChatDemo.jsx
+++ src/components/ChatDemo/ChatDemo.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import "./ChatDemo.css";
-import conversation from "./conversation";
+import conversation from "./conversation.js";
 
 
 function ChatDemo() {
